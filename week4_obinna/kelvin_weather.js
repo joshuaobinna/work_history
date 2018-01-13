70b71130dfdc0720851592ec98872deabc1da0eb
joshuaobinna <joshuaobinna93@gmail.com>
2018-01-13 15:55:09 +0100

    kelvin weather

---
+++ week4_obinna/kelvin_weather.js
@@ -0,0 +1,6 @@
+const kelvin = 294; console.log(kelvin); // todays forecast in kelvin.
+let celcius = kelvin - 273; console.log(celcius); // output for celcius.
+let fahrenheit = 21 * (9/5) + 32;
+console.log(Math.floor(fahrenheit));
+console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`); // lggin the temperature
+const kelvi = prompt('What is the kelvin temperature today?');
