bd789abfcfdb3d4715b7da2027a11e1ebe623fea
joshuaobinna <joshuaobinna93@gmail.com>
2018-01-13 15:58:47 +0100

    Dog years

---
+++ week4_obinna/dog_years.js
@@ -0,0 +1,18 @@
+let myAge = 26;
+console.log(myAge); // this is my age
+let earlyYears = 2;
+console.log(earlyYears); // output
+earlyYears *= 10.5;
+console.log(earlyYears);
+myAge -= 2;
+console.log(myAge); // new age
+let laterYears = 24;
+console.log(laterYears);
+laterYears *= 4; console.log(laterYears); // output: 96;
+earlyYears += laterYears;
+console.log(earlyYears); // output is 117
+let myAgeInDogYears = 117;
+console.log(myAgeInDogYears); //my new age in dog years.
+let myName = 'OBINNA'; console.log(myName);
+console.log('OBINNA'.toLowerCase()); // uppercases to lowercasses.
+console.log(`My name is $ {myName}. I am $ {myAgeInDogYears} years old in dog years .`)
