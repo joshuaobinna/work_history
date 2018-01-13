bb8e08aeb36ee52aeb482accfd979f87c3558585
joshuaobinna <joshuaobinna93@gmail.com>
2018-01-13 16:04:55 +0100

    majic balls

---
+++ week4_obinna/majic_eight_ball.js
@@ -0,0 +1,17 @@
+let userName = 'obinna';
+userName? console.log('Hello obinna'):
+console.log('Hello!');
+
+let randomNumber = 3;
+console.log(Math.floor(Math.random() * 7));
+eightBall = '';
+switch (randomNumber) {
+  case 3:
+    console.log('Cannot predict now');
+    break;
+let userQuestion = 'wll I become rich in future?';
+console.log('obinna asked' + userQuestion);
+console.log('The eight ball answered:' + eightBall);
+
+
+                    }
