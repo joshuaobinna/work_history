4ae93253ed6c511831009bfa4af8b85aedb26b24
joshuaobinna <joshuaobinna93@gmail.com>
2018-01-27 07:37:43 +0100

    Iterators

---
+++ Assessments/week5/3-iterators.js
@@ -0,0 +1,48 @@
+/**
+  In this assessment, you will use your knowledge of javascript iterator
+  to complete the code below. Array specific functions that will be used for this
+  assessment:
+    > forEach
+    > some
+    > filter
+    > map
+
+  ```
+  function {{name_of_function}} {
+    // Write your code here
+  }
+
+  OR
+
+  const {{name_of_function}} = () => {
+    // Write your code here
+  }
+
+  ```
+*/
+
+const week = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+// 1. Using the `forEach` function complete the instructions below:
+// Console.log every day in the week array
+  week.forEach(function(weekDays) {
+   console.log(' - ' + weekDays);
+ });
+
+// 2. Using the `some` function complete the instructions below:
+// Check the week array for days that are longer than six letters
+
+const hasDaysLongerThanSixLetters =   week.some(week => week.length > 6 );
+
+// 3. Using the `filter` function complete the instructions below:
+// Filter the weeks array for all the days that are longer than six letters
+
+const filteredDaysLongerThanSixLetters =  week.filter(week => week.length > 6);
+
+// 4. Using the `map` function complete the instructions below:
+// create a new array that has all the days captialized
+
+// HINT - use `toUpperCase` function to captialize strings
+// https://www.w3schools.com/jsref/jsref_touppercase.asp
+
+const arrayOfCapitalizedDays =  week.map(week => week.toUpperCase());
