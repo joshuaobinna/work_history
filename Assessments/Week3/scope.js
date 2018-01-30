ed6d15ba436ae69b9850ba8dba0d7cc755b3ae2c
Joshua Obinna <34477279+joshuaobinna@users.noreply.github.com>
2018-01-30 21:49:08 +0100

    scope.js

--- Assessments/Week3/scope.js
+++ Assessments/Week3/scope.js
@@ -4,55 +4,54 @@
 
 // What will the below functions console log?
 function run1() {
-    let a = 10;
-    if(a > 5) {
-        a = 7;
-    }
-    console.log(a);
+  let a = 10;
+  if(a > 5) {
+  a = 7;
+}
+  console.log(a)
 }
 run1()
-// 10
+//answer = 7
 
 function run2() {
-    if(true) {
-        const a = 5;
-    }
-    console.log(a);
+if(true) {
+const a = 5;
+}console.log(a);
 }
 run2()
-// undefined
+//  a is not defined
 
 function run3() {
-  const a = 2;
-  a = 3;
-  console.log(a);
+const a = 2;
+a = 3;
+console.log(a);
 }
 run3()
-// already assigned 2
+// a is already assigned 2
 
 // What will the below functions console log? And in what order?
 // ex: First => 10, Second => 4
 
 const a = 6;
 function run4() {
-    const a = 7;
-    console.log(a);
+const a = 7
+console.log(a);
 }
 run4();
 console.log(a);​
-// Answer = 7
+// first Answer = 7, second answer is 6
 
 const a = 6;
 function run5() {
-    const a = 7;
-    function run6() {
-        const a = 8;
-        console.log(a);  // 10
-    }
-    run6();
-    console.log(a);  // 4
+const a = 7;
+function run6() {
+const a = 8;
+console.log(a);  // 8
+}
+   run6();
+    console.log(a);  // 7
 }
 run5();
-console.log(a);​  // Third
+console.log(a);​  // 6
 
-// Answer = 7
+// Answer; first => 8, second => 7, third => 6
