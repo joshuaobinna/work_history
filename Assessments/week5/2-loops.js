114ba9b7ce2d0e83d2b8841645d8f0943adec19a
Joshua Obinna <34477279+joshuaobinna@users.noreply.github.com>
2018-02-09 16:38:57 +0100

    Update 2-loops.js

--- Assessments/week5/2-loops.js
+++ Assessments/week5/2-loops.js
@@ -17,20 +17,20 @@
 */
 
 // 1. write a function that takes an array as its first parameter and console logs
-// every item in the arra
-players = ['Messi', 'Ronaldo', 'Morata', 'Hazard', 'Ozil', 'Sanchez', 'Mikel'];
-football(players);
-function football(players) {
-  for (let playersIndex = 0; playersIndex < players.length; playersIndex++) {
-    console.log(players[playersIndex]);
+// every item in the array
+names = ['Messi', 'Ronaldo', 'Morata', 'Hazard', 'Ozil', 'Sanchez', 'Mikel'];
+getPlayers(names);
+function listPlayers(players) {
+  for (let namesIndex = 0; namesIndex < names.length; namesIndex++) {
+    console.log(names[namesIndex]);
   }
 }
 
 // 2. write a function that takes an array as its first parameter and console logs
 // every item with an odd index
 players = ['Messi', 'Ronaldo', 'Morata', 'Hazard', 'Ozil', 'Sanchez', 'Mikel'];
-football(players);
-function football(players) {
+getOddIndex(players);
+function getOddIndex(players) {
   for (let playersIndex = 0; playersIndex < players.length; playersIndex+=2) {
     console.log(players[playersIndex]);
   }
@@ -40,8 +40,8 @@ function football(players) {
 // 3. write a function that takes an array as its first parameter and console logs
 // every item with an even index
 players = ['Messi', 'Ronaldo', 'Morata', 'Hazard', 'Ozil', 'Sanchez', 'Mikel'];
-football(players);
-function football(players) {
+getEvenIndex(players);
+function getEvenIndex(players) {
   for (let playersIndex = 1; playersIndex < players.length; playersIndex+=2) {
     console.log(players[playersIndex]);
   }
@@ -52,41 +52,33 @@ function football(players) {
 // its second parameter. The function returns true if the searchValue is in the array
 // and false if it is not
 players = ['Messi', 'Ronaldo', 'Morata', 'Hazard', 'Ozil', 'Sanchez', 'Mikel'];
-searchValue = 'Messi';
-function football(players, searchValue) {
-  for (let playersIndex = 0; playersIndex < players.length; playersIndex++){
-    if (players[playersIndex] === searchValue){
-      console.log(true);
-      return true;
-    } else {console.log(false)
-    return false;
-   }
-  }
-}
+searchValue = 'Sanchez';
+function findSearchValue(players, searchValue) {
+return players.includes(searchValue);
+}//output: true
 
 // 5. write a function that takes an array as its first parameter, and a replaceValue as
 // its second parameter. The function returns a new array where all the values in the
 // array have been overwritten by the replaceValue.
-function variable(array,replaceValue){
-  for (let arrayIndex = 0; arrayIndex <= array.length; arrayIndex++){
-    if (arrayIndex !== replaceValue){
-      let r = array.indexOf(array[arrayIndex]);
-      array.splice(r, 1, replaceValue);
-       //the new array
-    } else {}
-  }
-  console.log(array);
-}variable(['herd', 'stranger', 'above',], 'beauty');
+array = ['A', 'B', 'C', 'D'];
+replaceValue = 'E';
+function overwriteArray(array,replaceValue){
+  for (let arrayIndex = 0; arrayIndex <= array.length; arrayIndex++);
+  array.splice(0, array.length, replaceValue);
+  let array = newArray;
+  return newArray;
+}
 
 // 6. write a function that takes an array as its first parameter, a searchValue as
 // its second parameter and a replaceValue as the third parameter. The function returns
 // a new array where only the searchValue has been overwritten by the replaceValue.
-function variable(array, searchValue, replaceValue){
+function overwriteSearchValue(array, searchValue, replaceValue){
   for (let arrayIndex = 0; arrayIndex <= array.length; arrayIndex++){
-    if (arrayIndex === searchValue){
+    if (array[arrayIndex] === searchValue){
       let r = array.indexOf(searchValue);
       array.splice(r, 1, replaceValue);
-      console.log(array); // the new array
-    } else {}
+      let array = newArray;
+      return newArray;
+    }
   }
-}variable(['herd', 'stranger', 'above',], 'stranger', 'beauty');
+}overwriteSearchValue(['herd', 'stranger', 'above',], 'stranger', 'beauty');
